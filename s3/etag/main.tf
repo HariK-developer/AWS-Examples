f9135aefa0b0ca41a99fc5365f75f89e5992745d
Hari <hariswdeveloper@gmail.com>
2025-12-31 07:34:36 +0530

    etags examples

---
+++ s3/etag/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  
+}
+
+resource "aws_s3_bucket" "default" {
+
+}
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.default.id
+  key    = "myfile.txt"
+  source = "myfile.txt"
+
+  etag = filemd5("myfile.txt")
+}
